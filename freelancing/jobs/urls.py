bc725e46cc4fcab1204fe9efee54d008dbb47c1c
SmithSparsha <SmithSparsha@DESKTOP-1I1BGCN>
2024-06-12 18:38:30 +0545

    forms for creating account

--- freelancing/jobs/urls.py
+++ freelancing/jobs/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index,SellerListView, SellerDetailView
+from .views import index,SellerListView, SellerDetailView,SellerCreateView,BuyerCreateView
 
 
 urlpatterns = [
     path('',SellerListView.as_view(), name='seller-list'),
     path('seller/<int:pk>/', SellerDetailView.as_view(), name= 'seller-detail'),
+    path('seller/create', SellerCreateView.as_view(), name='seller-createForm'),
+    path('buyer/create', BuyerCreateView.as_view(), name='buyer-createForm'),
 
 ]
--- freelancing/jobs/views.py
+++ freelancing/jobs/views.py
@@ -1,7 +1,9 @@
+from django.forms import BaseModelForm
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView,CreateView
+from django.urls import reverse_lazy
 
-from jobs.models import Seller
+from jobs.models import Seller, Buyer
 
 def index(request):
     return HttpResponse("hehe")
@@ -12,3 +14,21 @@ class SellerListView(ListView):
 
 class SellerDetailView(DetailView):
     model = Seller
+
+class SellerCreateView(CreateView):
+    model = Seller
+    fields = ['name', 'tagline','bio','website']
+    success_url = reverse_lazy('Seller-list')
+
+    def form_valid(self, form):
+        form.instance.owner =self.request.user
+        return super(SellerCreateView,self).form_valid(form)
+    
+class BuyerCreateView(CreateView):
+    model = Buyer
+    fields = ['name','bio']
+    success_url = reverse_lazy('Seller-list')
+
+    def form_valid(self, form):
+        form.instance.owner =self.request.user
+        return super(SellerCreateView,self).form_valid(form)

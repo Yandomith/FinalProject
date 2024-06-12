1999f37039eaae9448514570156396334688a3f7
SmithSparsha <SmithSparsha@DESKTOP-1I1BGCN>
2024-06-12 18:46:30 +0545

    more

--- freelancing/jobs/urls.py
+++ freelancing/jobs/urls.py
@@ -5,7 +5,7 @@ from .views import index,SellerListView, SellerDetailView,SellerCreateView,Buyer
 urlpatterns = [
     path('',SellerListView.as_view(), name='seller-list'),
     path('seller/<int:pk>/', SellerDetailView.as_view(), name= 'seller-detail'),
-    path('seller/create', SellerCreateView.as_view(), name='seller-createForm'),
-    path('buyer/create', BuyerCreateView.as_view(), name='buyer-createForm'),
+    path('seller/create', SellerCreateView.as_view(), name='seller-create'),
+    path('buyer/create', BuyerCreateView.as_view(), name='buyer-create'),
 
 ]
